6c9a4bfb2e59b031beb2f14514cc7293450745b1
cool guy <133474921+ameenTheprogramer@users.noreply.github.com>
2024-02-05 12:34:17 +0530

    Update script.py

--- script.py
+++ script.py
@@ -11,20 +11,4 @@ subprocess.run(['pip', 'install', 'pyautogui'])
 # Download the arrow_key_press.py script
 subprocess.run(['wget', 'https://raw.githubusercontent.com/ameenTheprogramer/right_arrow/main/arrow_key_press.py'])
 
-# Print the value of DISPLAY
-display_value = subprocess.run(['echo', '$DISPLAY'], capture_output=True, text=True).stdout.strip()
-print(f"DISPLAY value: {display_value}")
-
-
-os.environ['DISPLAY'] = display_value
-
-# Create and set permissions for .Xauthority file
-subprocess.run(['touch', '~/.Xauthority'])
-subprocess.run(['chmod', '600', '~/.Xauthority'])
-
-# Export DISPLAY=:0
-subprocess.run(['export', 'DISPLAY=:0'])
-
-# Exit the current terminal session
-subprocess.run(['exit'])
 
