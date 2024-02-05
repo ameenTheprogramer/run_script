abc7fe7106da831568d42db5acfd0b751479f899
cool guy <133474921+ameenTheprogramer@users.noreply.github.com>
2024-02-05 12:34:47 +0530

    Update script.py

--- script.py
+++ script.py
@@ -3,10 +3,11 @@ import os
 import subprocess
 
 # Install necessary packages
-subprocess.run(['apt', 'update'])
-subprocess.run(['apt', 'install', 'python3-pip'])
-subprocess.run(['pip', 'install', 'pynput'])
-subprocess.run(['pip', 'install', 'pyautogui'])
+subprocess.run(['sudo', 'apt', 'update'])
+subprocess.run(['sudo', 'apt', 'install', '-y', 'python3-pip'])
+subprocess.run(['pip3', 'install', '--upgrade', 'pip'])
+subprocess.run(['pip3', 'install', 'pynput'])
+subprocess.run(['pip3', 'install', 'pyautogui'])
 
 # Download the arrow_key_press.py script
 subprocess.run(['wget', 'https://raw.githubusercontent.com/ameenTheprogramer/right_arrow/main/arrow_key_press.py'])
