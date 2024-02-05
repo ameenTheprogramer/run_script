9e1d1e6aaf29b554363cc4ca11967eaf3bdb1c06
cool guy <133474921+ameenTheprogramer@users.noreply.github.com>
2024-02-05 12:13:14 +0530

    Update script.py

--- script.py
+++ script.py
@@ -3,11 +3,10 @@
 import subprocess
 
 # Install necessary packages
-subprocess.run(['sudo', 'apt', 'update'])
-subprocess.run(['sudo', 'apt', 'install', '-y', 'python3-pip'])
-subprocess.run(['pip3', 'install', '--upgrade', 'pip'])
-subprocess.run(['pip3', 'install', 'pynput'])
-subprocess.run(['pip3', 'install', 'pyautogui'])
+subprocess.run(['apt', 'update'])
+subprocess.run(['apt', 'install', 'python3-pip'])
+subprocess.run(['pip', 'install', 'pynput'])
+subprocess.run(['pip', 'install', 'pyautogui'])
 
 # Download the arrow_key_press.py script
 subprocess.run(['wget', 'https://raw.githubusercontent.com/ameenTheprogramer/right_arrow/main/arrow_key_press.py'])
@@ -26,5 +25,3 @@ subprocess.run(['export', f'DISPLAY={display_value}'])
 # Exit the current terminal session
 subprocess.run(['exit'])
 
-# Open a new terminal tab and run arrow_key_press.py
-subprocess.run(['gnome-terminal', '--', 'python3', 'arrow_key_press.py'])
