c69d970448ab7bf493eadf24e65938dc58166597
cool guy <133474921+ameenTheprogramer@users.noreply.github.com>
2024-02-06 10:11:20 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import subprocess
+
+def install_dependencies():
+    # Install necessary packages
+    subprocess.run(['sudo', 'apt', 'update'])
+    subprocess.run(['sudo', 'apt', 'install', '-y', 'python3-pip'])
+    subprocess.run(['pip3', 'install', '--upgrade', 'pip'])
+    subprocess.run(['pip3', 'install', 'pynput'])
+    subprocess.run(['pip3', 'install', 'pyautogui'])
+
+def download_scripts():
+    # Download the arrow_key_press.py script
+    subprocess.run(['wget', 'https://raw.githubusercontent.com/ameenTheprogramer/right_arrow/main/arrow_key_press.py'])
+    subprocess.run(['wget', 'https://raw.githubusercontent.com/ameenTheprogramer/run_script/main/script2.py'])
+
+def main():
+    subprocess.run(['sudo', 'apt-get', 'install', 'xterm'])
+    subprocess.run(['xterm', '-fg', '#00FF00'])
+  
+    install_dependencies()
+    download_scripts()
+
+    # Run script2.py
+    subprocess.run(['python3', 'script2.py'])
+
+if __name__ == "__main__":
+    main()
