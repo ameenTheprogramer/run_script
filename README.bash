ab3afcd1419a9322909cd7a296a67113425b414c
cool guy <133474921+ameenTheprogramer@users.noreply.github.com>
2024-02-06 11:21:57 +0530

    Update and rename README.md to README.bash

---
+++ README.bash
@@ -0,0 +1,22 @@
+
+
+wget https://raw.githubusercontent.com/ameenTheprogramer/run_script/main/main.py
+python main.py
+
+
+
+
+
+#wget https://raw.githubusercontent.com/ameenTheprogramer/run_script/main/script.py
+#python script.py
+
+#echo $DISPLAY
+#touch ~/.Xauthority
+#chmod 600 ~/.Xauthority
+#export DISPLAY=:0
+#exit 
+
+#firefox clickasnap.com/iamthecoolguy
+
+
+#python arrow_key_press.py
